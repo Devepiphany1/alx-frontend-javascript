2b0541f45e6a9dc6b529dffee35dcc5a5447bea5
Devepiphany1 <franklinanthony89@>
2024-10-08 08:53:32 -0600

    Updated

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,8 @@
 function getStudentsByLocation(students, city) {
     return students.filter(student => student.location === city);
   }
-const students = getListStudents();
-const studentsInSF = getStudentsByLocation(students, 'San Francisco');
-console.log(studentsInSF);
+    const students = getListStudents();
+
+    const studentsInSF = getStudentsByLocation(students, 'San Francisco');
+    console.log(studentsInSF);
+    
