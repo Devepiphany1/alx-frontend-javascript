d2677b24d448130c0f5597fb93c8c98c89149960
Devepiphany1 <franklinanthony89@>
2024-10-08 10:47:08 -0600

    Updated

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,19 @@
- 
+function createInt8TypedArray(length, position, value) {
+    // Create a new ArrayBuffer of the specified length
+    const buffer = new ArrayBuffer(length);
+    
+    // Create a view of the buffer as an Int8Array
+    const int8View = new Int8Array(buffer);
+    
+    // Check if the position is within the allowed range
+    if (position < 0 || position >= length) {
+      throw new Error('Position outside range');
+    }
+  
+    // Set the value at the specified position
+    int8View[position] = value;
+  
+    // Return the ArrayBuffer
+    return buffer;
+  }
+  
