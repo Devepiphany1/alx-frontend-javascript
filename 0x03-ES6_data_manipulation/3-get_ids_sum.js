6f40eb724b86966337b802d8f35d93571cf5950a
Devepiphany1 <franklinanthony89@>
2024-10-08 09:37:30 -0600

    Updated

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,6 +2,6 @@ function getStudentIdsSum(students) {
     return students.reduce((sum, student) => sum + student.id, 0);
   }
 
-const students = getListStudents();
-const totalIdsSum = getStudentIdsSum(students);
+
 console.log(totalIdsSum);
+getStudentIdsSum();
